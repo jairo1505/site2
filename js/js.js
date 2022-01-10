225d73955867cfb979363c7188529fdc8a6e68c3
jairo1505 <jairojr.p@hotmail.com>
2022-01-10 12:25:26 -0300

    Update contact

--- js/js.js
+++ js/js.js
@@ -40,32 +40,15 @@ function contato(){
 	document.getElementById("MenuSobre").className = "";
 	document.getElementById("MenuContato").className = "active";
 	document.getElementById("MenuSuporte").className = "";
-	var largura = window.innerWidth;
-	if (largura < 800) {
-			result.innerHTML = "<div class=\"contato\">";
-			result.innerHTML += "<h3 class=\"left\">Contato:</h3>";
-			result.innerHTML += "<p class=\"left\">Jaraguá Telecomunicações Ltda - Comércio e Assistência Técnica</p>";
-			result.innerHTML += "<p class=\"left\">Rua Bernardo Dornbusch, 590 - Sala 02 - Baependi</p>";
-			result.innerHTML += "<p class=\"left\">89256-100 - Jaraguá do Sul - SC</p>";
-			result.innerHTML += "<p class=\"left\">Fone: 47 3275-2444</p>";
-			result.innerHTML += "<p class=\"left\">email: jaraguatel@netuno.com.br</p>";
-			result.innerHTML += "<h3 class=\"left\">Horário de funcionamento:</h3>";
-			result.innerHTML += "<p class=\"left\">De segunda à sexta das 7:30h às 12h e das 13:30h às 17:48h</p>";
-			result.innerHTML += "<iframe class=\"map\" src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d842.6857610437048!2d-49.07337271952065!3d-26.482224062623224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94de9505c17ec6f1%3A0x12ad5ff13f11f847!2sR.+Bernardo+Dornbusch%2C+590+-+Vila+Baependi%2C+Jaragu%C3%A1+do+Sul+-+SC%2C+89256-100!5e0!3m2!1spt-BR!2sbr!4v1552675831499\" width=\"100%\" height=\"350\" frameborder=\"0\" style=\"border:0\" align=\"center\" allowfullscreen></iframe>";
-			result.innerHTML += "<hr/></div>";
-		}else{
-			result.innerHTML = "<div class=\"contato\">";
-			result.innerHTML += "<iframe class=\"map\" src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d842.6857610437048!2d-49.07337271952065!3d-26.482224062623224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94de9505c17ec6f1%3A0x12ad5ff13f11f847!2sR.+Bernardo+Dornbusch%2C+590+-+Vila+Baependi%2C+Jaragu%C3%A1+do+Sul+-+SC%2C+89256-100!5e0!3m2!1spt-BR!2sbr!4v1552675831499\" width=\"450\" height=\"350\" frameborder=\"0\" style=\"border:0\" align=\"right\" allowfullscreen></iframe>";
-			result.innerHTML += "<h3 class=\"left\">Contato:</h3>";
-			result.innerHTML += "<p class=\"left\">Jaraguá Telecomunicações Ltda - Comércio e Assistência Técnica</p>";
-			result.innerHTML += "<p class=\"left\">Rua Bernardo Dornbusch, 590 - Sala 02 - Baependi</p>";
-			result.innerHTML += "<p class=\"left\">89256-100 - Jaraguá do Sul - SC</p>";
-			result.innerHTML += "<p class=\"left\">Fone: 47 3275-2444</p>";
-			result.innerHTML += "<p class=\"left\">email: jaraguatel@netuno.com.br</p>";
-			result.innerHTML += "<h3 class=\"left\">Horário de funcionamento:</h3>";
-			result.innerHTML += "<p class=\"left\">De segunda à sexta das 7:30h às 12h e das 13:30h às 17:48h</p>";
-			result.innerHTML += "</div><br/><br/><br/><br/>";
-		}
+	
+	result.innerHTML = "<div class=\"contato\">";
+	result.innerHTML += "<h3>Contato:</h3>";
+	result.innerHTML += "<p><font color=\"red\">Informamos que desde 24/12/2021 não dispomos mais de endereço físico.</font></p>";
+	result.innerHTML += "<p>Telefone/WhatsApp: <a href=\"tel:4732752444\">(47) 3275-2444</a></p>";
+	result.innerHTML += "<p>E-mail: <a href=\"mailto:jaraguatel@netuno.com.br\">jaraguatel@netuno.com.br</a></p>";
+	result.innerHTML += "<h3>Horário de funcionamento:</h3>";
+	result.innerHTML += "<p>De segunda à sexta das 9h às 12h e das 13h às 17h</p>";
+	result.innerHTML += "</div><br/><br/><br/><br/>";
 }
 function suporte(){
 	show();
